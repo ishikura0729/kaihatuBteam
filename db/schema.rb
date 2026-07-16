e6cc2f1db0a69a4a7b9d37de3ff24979bdeea423
s-oono01 <r7_oono@icc.core.ac.jp>
2026-07-16 11:38:53 +0900

    お知らせ一覧の画面を変えました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_02_004520) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_02_044844) do
   create_table "announcements", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -18,5 +18,42 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_02_004520) do
     t.datetime "updated_at", null: false
   end
 
-end
+  create_table "shift_exchange_requests", force: :cascade do |t|
+    t.integer "requester_id"
+    t.integer "partner_id"
+    t.integer "requester_shift_id"
+    t.integer "partner_shift_id"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
+  create_table "shift_requests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.date "requested_date"
+    t.time "start_time"
+    t.time "end_time"
+    t.text "reason"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shift_requests_on_user_id"
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.integer "user_id"
+    t.date "date"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "shift_requests", "users"
+end
