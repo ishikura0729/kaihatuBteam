339759170d7e5f5f4cc195e9c68dafc672fbb725
jodai-1 <jodai@icc.core.ac.jp>
2026-07-16 10:33:25 +0900

    お知らせroutes.rbの変更をした

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :announcements
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :announcements, only: [:index, :show, :new, :create]
+  resources :announcements, only: [:index, :show, :new, :create, :edit]
   root "announcements#index"
 end
 
