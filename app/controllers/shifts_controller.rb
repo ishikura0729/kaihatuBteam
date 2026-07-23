fb3d3e70dc4272f53002907e925f2e2c5fa2e646
s-ishikura01 <R7_ishikura@icc.core.ac.jp>
2026-07-23 15:38:20 +0900

    完成！

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,7 +1,8 @@
 class ShiftsController < ApplicationController
   
-  before_action :manager_only, except: [:index], :require_login  
-  
+before_action :require_login
+before_action :manager_only,except: [:index]
+
   def index
     @shifts = Shift.all
   end
@@ -92,4 +93,6 @@ class ShiftsController < ApplicationController
       :login_id, :name, :work_date, :start_time, :end_time
     )
   end
+
+
 end
