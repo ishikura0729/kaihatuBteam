9bb5d1a46d01125640af3ed58f5ecec9725ef082
s-ishikura01 <R7_ishikura@icc.core.ac.jp>
2026-07-23 14:18:08 +0900

    い

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
 
   get 'sessions/new'
-  delete "logout", to: "sessionsdestroy"
+  delete "logout", to: "sessions#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "login" , to: "sessions#new"
