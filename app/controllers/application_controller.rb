f4912974f0e5ee64aa93e8b92c3de9a52095c86b
s-ishikura01 <R7_ishikura@icc.core.ac.jp>
2026-07-23 15:14:34 +0900

    え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  private
+
+    def manager_only
+        unless current_user&.role == 1
+            redirect_to announcements_path, alert: "権限がありません。"
+        end
+    end
+
 end
