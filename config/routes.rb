ae2a97da18266b8c571d6c16e796f0c776619c8d
s-ishikura01 <R7_ishikura@icc.core.ac.jp>
2026-07-23 11:02:36 +0900

    画面遷移途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
 
-  resources :shifts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "login" , to: "sessions#new"
+  post "login",to: "sessions#create"
   resources :users
-  
+  resources :announcements, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :shifts
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -12,7 +15,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :announcements, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   root "announcements#index"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_16_000000) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_16_065252) do
   create_table "announcements", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_16_000000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "login_id"
-    t.integer "role", default: 2
+    t.integer "role"
   end
 
   add_foreign_key "shift_requests", "users"
